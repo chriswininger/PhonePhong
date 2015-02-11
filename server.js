6b853448f375dc48a645c10dc5ce326cd8cafd02
chriswininger <cwininger@airspringsoftware.com>
2015-02-11 09:11:00 -0500

    added comments and renamed some files

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/**
+ * Simple test server
+ *  This can probably be removed from the final project but it simplifies testing
+ *  by letting you easily host the public directory of this project over the network
+ *  so mobile devices can reach it
+ * @type {*|exports}
+ */
+
 var express = require('express');
 var app = express();
 
